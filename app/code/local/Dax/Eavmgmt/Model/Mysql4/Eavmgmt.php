a855ddea9a9f943568cb326d2e843aa4275e0aaa
daxpanara7 <daxpanara7@gmail.com>
2023-05-29 12:14:46 +0530

    Task 0.3.0

--- app/code/local/Dax/Eavmgmt/Model/Mysql4/Eavmgmt.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Dax_Eavmgmt_Model_Mysql4_Eavmgmt extends Dax_Eavmgmt_Model_Resource_Eavmgmt
-{
-
-}
--- app/code/local/Dax/Eavmgmt/Model/Mysql4/Eavmgmt/Collection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Dax_Eavmgmt_Model_Mysql4_Eavmgmt_Collection extends Dax_Eavmgmt_Model_Resource_Eavmgmt_Collection
-{
-
-}
-
-?>
