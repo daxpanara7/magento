0b6126b626727a05f269c78d056b5c7766522d43
daxpanara7 <daxpanara7@gmail.com>
2023-06-23 16:29:24 +0530

    practice-module

---
+++ app/code/local/Ccc/Practice/controllers/Adminhtml/Catalog/ProductController.php
@@ -0,0 +1,10 @@
+<?php
+class Ccc_Practice_Adminhtml_Catalog_ProductController extends Mage_Adminhtml_Controller_Action
+{
+	public function indexAction()
+	{
+		// echo "I am in Product Controller";
+	}
+}
+
+?>
